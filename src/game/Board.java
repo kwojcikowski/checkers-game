153290ea1daf7f7ec291de0790630440b2a910b4
Damian Garbala <garbalad@gmail.com>
2019-09-24 13:31:24 +0200

    updated setboard

--- src/game/Board.java
+++ src/game/Board.java
@@ -17,11 +17,11 @@ public class Board {
 
         for(int i=0;i<4;i++)
             for(int j=0;j<8;j++)
-                if((j+i)%2==0) board[i][j].setOccupant(new Piece()); //for white
+                if((j+i)%2==0) board[i][j].setOccupant(new Piece(true)); //for white
 
         for(int i=8;i>5;i--)
             for(int j=0;j<8;j++)
-                if((j+i)%2==0) board[i][j].setOccupant(new Piece()); //for black
+                if((j+i)%2==0) board[i][j].setOccupant(new Piece(false)); //for black
     }
 
     public void startGame(){
