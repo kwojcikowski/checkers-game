b312f2da0e524caf086c81c81575aa13a716f019
Kacper Wojcikowski <wojcikowskikacper5@gmail.com>
2019-09-26 02:07:41 +0200

    added basic move functionality and linked with gui

--- src/game/Controller.java
+++ src/game/Controller.java
@@ -1,15 +1,24 @@
 package game;
 
+import javafx.animation.PathTransition;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.util.Duration;
 import pieces.Piece;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 public class Controller {
 
@@ -17,6 +26,7 @@ public class Controller {
     @FXML private GridPane boardGrid;
     @FXML private GridPane opponentTaken;
     @FXML private GridPane playerTaken;
+    @FXML private VBox boardWrap;
     private Tile[][] board;
     private Image whitePieceImg;
     private Image blackPieceImg;
@@ -55,18 +65,23 @@ public class Controller {
         }
 
         //Placing pieces
-        for(int i = 0; i < board.length; i++){
-            for(int j = 0; j < board[i].length; j++) {
+        for(int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
                 Tile t = board[i][j];
-                if(t.isOccupied()){
+                if (t.isOccupied()) {
                     Piece occupant = t.getOccupant();
-                    if(occupant.isWhite()){
+                    if (occupant.isWhite()) {
                         boardLayout[i][j].getChildren().clear();
                         ImageView view = new ImageView(whitePieceImg);
                         boardLayout[i][j].getChildren().add(view);
-                    }else{
+                        VBox box = boardLayout[i][j];
+                        int finalI = i;
+                        int finalJ = j;
+                        view.setOnMouseClicked(mouseEvent -> setupFields(finalI, finalJ));
+                    } else {
                         boardLayout[i][j].getChildren().clear();
                         ImageView view = new ImageView(blackPieceImg);
+                        view.toFront();
                         boardLayout[i][j].getChildren().add(view);
                     }
                 }
@@ -85,5 +100,93 @@ public class Controller {
             view.setImage(img);
             opponentTaken.add(view, 0, i);
         }*/
+    }
+
+    public void movePiece(VBox view, int i, int j){
+        int currentI = boardGrid.getRowCount() - GridPane.getRowIndex(view) -1;
+        int currentJ = GridPane.getColumnIndex(view);
+
+        boardGrid.getChildren().remove(view);
+        VBox newBox = new VBox();
+        boardLayout[currentI][currentJ] = newBox;
+        newBox.setAlignment(Pos.CENTER);
+        newBox.setId("blackTile");
+        boardGrid.add(newBox, GridPane.getColumnIndex(view), GridPane.getRowIndex(view));
+
+        view.setId("TravellingPiece");
+        container.getChildren().add(view);
+        double x = newBox.getLayoutX() + boardWrap.getLayoutX() + (whitePieceImg.getWidth() * Math.sqrt(2)/2)
+                + boardWrap.getPadding().getLeft()/2;
+        double y = newBox.getLayoutY() + boardWrap.getLayoutY() + (whitePieceImg.getHeight() * Math.sqrt(2)/2)
+                + boardWrap.getPadding().getTop()/2;
+        double endX;
+        if(currentJ < j) {
+            endX = x + (view.getWidth() * Math.sqrt(2) / 3 * 2) + boardWrap.getBorder().getInsets().getLeft();
+        }else{
+            endX = x - (view.getWidth() * Math.sqrt(2) / 3 * 2) - boardWrap.getBorder().getInsets().getLeft();
+        }
+        double endY = y - (view.getHeight() * Math.sqrt(2)/3 * 2) + boardWrap.getBorder().getInsets().getTop();
+
+        //Transition effect
+        Line line = new Line(x, y, endX, endY);
+
+        PathTransition transition = new PathTransition();
+        transition.setNode(view);
+        transition.setDuration(Duration.seconds(0.3));
+        transition.setPath(line);
+        transition.setOnFinished(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                exchangeBoxes(view, i, j);
+            }
+        });
+        transition.play();
+    }
+
+    public void exchangeBoxes(VBox view, int i, int j){
+        updateOnMouse();
+        VBox temp = boardLayout[i][j];
+        int valI = GridPane.getColumnIndex(temp);
+        int valJ = GridPane.getRowIndex(temp);
+        temp.setId("blackTile");
+        temp.getChildren().addAll(view.getChildren());
+        temp.getChildren().get(0).setOnMouseClicked(mouseEvent -> setupFields(i, j));
+        System.out.println(temp.getChildren().get(0));
+        System.out.println(temp);
+        System.out.println(boardLayout[i][j]);
+        boardGrid.getChildren().remove(temp);
+        boardGrid.getChildren().add(temp);
+        GridPane.setConstraints(temp, valI, valJ);
+        container.getChildren().remove(view);
+    }
+
+    public void setupFields(int i, int j){
+        updateOnMouse();
+        if(i+1 < 8 && j+1 <8){
+            boardLayout[i+1][j+1].setOnMouseClicked(mouseEvent -> {
+                movePiece(boardLayout[i][j], i+1, j+1);
+            });
+            boardLayout[i+1][j+1].setId("available");
+        }
+        if(i+1 < 8 && j-1 > -1){
+            boardLayout[i+1][j-1].setOnMouseClicked(mouseEvent -> {
+                movePiece(boardLayout[i][j], i+1, j-1);
+            });
+            boardLayout[i+1][j-1].setId("available");
+        }
+    }
+
+    public void updateOnMouse(){
+        for(int i = 0; i < boardLayout.length; i++) {
+            for (int j = 0; j < boardLayout[i].length; j++) {
+                VBox box = boardLayout[i][j];
+                if(box.getId().equalsIgnoreCase("available")){
+                    box.setId("blackTile");
+                    int finalI = i;
+                    int finalJ = j;
+                    box.setOnMouseClicked(mouseEvent -> setupFields(finalI, finalJ));
+                }
+            }
+        }
     }
 }
