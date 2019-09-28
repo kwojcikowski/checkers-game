b4e26ae2b1737a0a25479e180e65240a3e6dc9be
Damian Garbala <garbalad@gmail.com>
2019-09-28 13:34:19 +0200

    major changes in movechecker method

--- src/game/Game.java
+++ src/game/Game.java
@@ -15,28 +15,32 @@ public class Game {
         return board;
     }
 
-    public LinkedList<Move> moveChecker(int x, int y){
-        final Tile[][] tiles=getBoard().getBoard();
+    public LinkedList<Move> moveChecker(int x, int y, final Tile[][] tiles){
         LinkedList<Move> moves=new LinkedList<>();
         final boolean isWhite=tiles[x][y].getOccupant().isWhite();
-        if(isWhite){
-            if(y+1<8){
-                for(int i=-1;i<3;i+=2){
-                    try{
-                        if(!tiles[x+i][y+1].isOccupied()){
-                            moves.add(new Move(x+i,y+1,false));
-                        }
-                        else if(tiles[x+i][y+1].isOccupied()&&isWhite!=tiles[x+i][y+1].getOccupant()
-                                .isWhite()){
-                            if(/*y+2<8&&x+2*i<8&&*/!tiles[x+i*2][y+2].isOccupied()){
-                                moves.add(new Move(x+i*2,y+2,true));
-                                moves.addAll(moveChecker(x+i*2,y+2));
-                            }
-                        }
-                    }catch(ArrayIndexOutOfBoundsException e){}
+        final int j;
+
+        if(isWhite) j=1;
+        else j=-1;
+
+        for(int i=-1;i<3;i+=2){
+            try{
+                if(!tiles[x+i][y+j].isOccupied()){
+                    moves.add(new Move(x+i,y+j,false));
                 }
+                else if(tiles[x+i][y+j].isOccupied()&&isWhite!=tiles[x+i][y+j].getOccupant().isWhite()){
+                    if(!tiles[x+i*2][y+2*j].isOccupied()){
+                        moves.add(new Move(x+i*2,y+2*j,true));
+                        Tile[][] alternative=tiles.clone(); //hope this is not a shallow copy
+                        alternative[x+i][y+j].setOccupant(null);
+                        moves.addAll(moveChecker(x+i*2,y+2*j,alternative));
+                    }
+                }
+            }catch(ArrayIndexOutOfBoundsException e){
+                continue;
             }
         }
+
         return moves;
     }
 
